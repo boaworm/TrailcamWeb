ce10c5389722730d50b973435b60b0ecd6d593f9
Henrik Thorburn <boaworm@gmail.com>
2024-01-18 16:34:37 -0600

    adding stacked bars. cleanup pending

--- shared.js
+++ shared.js
@@ -1,5 +1,6 @@
 // First graph - Observation per month 
 // Declare the chart dimensions and margins.
+
 function transformMapToD3DataArray(theMap) {
     m = [];
     for( const [key, value] of theMap.entries()){
@@ -10,10 +11,130 @@ function transformMapToD3DataArray(theMap) {
     }
     return m;
 }
-
+const cameraArray = ["TOP","MIDDLE","HOUSE"];
 const monthArray = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 function numericToTextMonth(month){
 	return monthArray[ month -1 ];
+}
+
+// Repeatable graph creation, StackedHBAR
+function createStackedHBarGraph(divName, title, dataObject, barLabel, groupLabel, valueLabel,  widthScale = 1, heightScale = 1){
+    let margin = {top: 30, right: 30, bottom: 70, left: 60},
+	width = (460*widthScale) - margin.left - margin.right,
+	height = (400*heightScale) - margin.top - margin.bottom;
+	// expect obj { barLabel: ... , groupLabel: ... , valueLabel: ... } 
+	// for ex	{ year: 2010, camera: "TOP", observations: 100 }
+
+	//console.log(dataObject);
+	// figure out X labels, and combined max X label (for Y scale)
+	var distinctBarLabels = [];
+	dataObject.forEach( (row) => {
+		if( distinctBarLabels.indexOf(row[barLabel]) === -1 ){
+			distinctBarLabels.push( row[barLabel] );
+		}
+	});
+	//console.log("distinct bar labels: ", distinctBarLabels);
+	var distinctGroupLabels = [];
+	dataObject.forEach( (row) =>{
+		if(distinctGroupLabels.indexOf(row[groupLabel]) === -1){
+			distinctGroupLabels.push(row[groupLabel]);
+		}
+	});
+	//console.log("distinct group labels: ", distinctGroupLabels);
+
+	let maxPerRow = 0;
+	distinctBarLabels.forEach( (row) => {
+		const subset = dataObject.filter( function(d) {
+			return d[barLabel] === row;
+		});
+		let sum = subset.reduce( function(a,d){
+			return a + +(d[valueLabel]);
+		},0);
+		if(sum > maxPerRow){
+			maxPerRow = sum;
+		}
+	});
+
+	//console.log("Max accumulated value for one row is: " , maxPerRow);
+
+     // append the svg object to the body of the page
+    let svg = d3.select(divName)
+		.append("svg")
+		.attr("width", width + margin.left + margin.right)
+		.attr("height", height + margin.top + margin.bottom)
+		.append("g")
+			.attr("transform",
+				"translate(" + margin.left + "," + margin.top + ")");
+
+    // Specific to Stacked BHar
+	var color = d3.scaleOrdinal()
+		.domain(distinctBarLabels)
+		.range(['#e41a1c','#377eb8','#4daf4a'])
+
+	// Add X axis
+	var x = d3.scaleBand()
+		.domain(distinctBarLabels)
+		.range([0, width])
+		.padding([0.2])
+		svg.append("g")
+			.attr("transform", "translate(0," + height + ")")
+			.call(d3.axisBottom(x).tickSizeOuter(0));
+	// Add Y axis
+	var y = d3.scaleLinear()
+		.domain([0, Math.ceil( 1.1 * maxPerRow)])
+		.range([ height, 0 ]);
+	svg.append("g")
+		.call(d3.axisLeft(y));
+
+	var stackedData = d3.stack()
+		.keys(d3.union(dataObject.map(d => d.camera)))
+		.value(([,group], key) => group.get(key).observations)
+		(d3.index(dataObject, d => d.year, d => d.camera));
+
+	// Show the bars
+	svg.append("g")
+		.selectAll("g")
+		// Enter in the stack data = loop key per key = group per group
+		.data(stackedData)
+		.enter().append("g")
+			.attr("fill", function(d) { return color(d.key); })
+			.selectAll("rect")
+			// enter a second time = loop subgroup per subgroup to add all rectangles
+			.data(function(d) { return d; })
+				.enter().append("rect")
+					.attr("x", function(d) { return x(d.data[0]); })
+					.attr("y", function(d) { return y(d[1]); })
+					.attr("height", function(d) { return y(d[0]) - y(d[1]); })
+					.attr("width",x.bandwidth());
+
+	var keys = distinctGroupLabels;
+	svg.selectAll("mydots")
+		.data(keys)
+		.enter()
+		.append("circle")
+			.attr("cx", function(d,i){ return 30 + i*100;})
+			.attr("cy", function(d,i){ return height+30})
+			.attr("r", 7)
+			.style("fill", function(d){ return color(d)})
+
+	svg.selectAll("mylabels")
+		.data(keys)
+		.enter()
+		.append("text")
+			.attr("x", function(d,i){ return 45 + i*100;})
+			.attr("y", function(d,i){ return height+30;}) 
+			.style("fill", function(d){ return color(d)})
+			.text(function(d){ return d})
+			.attr("text-anchor", "left")
+			.style("alignment-baseline", "middle");
+
+
+    svg.append("text")
+		.attr("x", (width / 2))
+		.attr("y", 0 - (margin.top / 2))
+		.attr("text-anchor", "middle")
+		.style("font-size", "16px")
+		.text(title);
 }
 
 // Repeatable graph creation, HBAR
@@ -74,4 +195,3 @@ function createHBarGraph(divName, title, dataObject, leftKey, rightKey, widthSca
 	.text(title);
 }
 
-///////// BEGIN MAIN DATA PROCESSING ////////////
