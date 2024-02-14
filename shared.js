604f548dc6c5bb09ba55baa3ff06c6378b360c67
Henrik Thorburn <boaworm@gmail.com>
2024-02-14 14:47:04 -0600

    adding three pie-charts in manual tab

--- shared.js
+++ shared.js
@@ -286,3 +286,69 @@ function createHBarGraph(divName, title, dataObject, leftKey, rightKey, widthSca
 		.text(title);
 }
 
+
+function createPieChart(divName, title, dataObject){
+const width = 450,
+    height = 450,
+    margin = 40;
+
+// The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
+const radius = Math.min(width, height) / 2 - margin
+
+// append the svg object to the div called 'my_dataviz'
+const svg = d3.select(divName)
+  .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+  .append("g")
+    .attr("transform", `translate(${width / 2}, ${height / 2})`);
+
+const data = {};
+for(const [key,value] of Object.entries(dataObject)){
+	data[ value.category ] = +value.count;
+}
+
+// set the color scale
+const color = d3.scaleOrdinal()
+  .range(d3.schemeSet2);
+
+// Compute the position of each group on the pie:
+const pie = d3.pie()
+  .value(function(d) {return d[1]});
+
+const data_ready = pie(Object.entries(data));
+
+// shape helper to build arcs:
+const arcGenerator = d3.arc()
+  .innerRadius(0)
+  .outerRadius(radius);
+
+// Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+svg
+  .selectAll('mySlices')
+  .data(data_ready)
+  .join('path')
+    .attr('d', arcGenerator)
+    .attr('fill', function(d){ return(color(d.data[0])) })
+    .attr("stroke", "black")
+    .style("stroke-width", "2px")
+    .style("opacity", 0.7);
+
+// Now add the annotation. Use the centroid method to get the best coordinates
+svg
+  .selectAll('mySlices')
+  .data(data_ready)
+  .join('text')
+  .text(function(d){ return d.data[0]})
+  .attr("transform", function(d) { return `translate(${arcGenerator.centroid(d)})`})
+  .style("text-anchor", "middle")
+  .style("font-size", 17);
+
+    svg.append("text")
+		.attr("x",1) 
+		.attr("y", -200 )
+		.attr("text-anchor", "middle")
+		.style("font-size", "16px")
+		.text(title);
+
+}
